7ce7c823404cec65d033a0cbef009497479a224e
riss-study <dlrudghks651@naver.com>
2023-05-10 17:00:17 +0900

    Initial commit

---
+++ src/test/java/dev/riss/springtx/SpringtxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.riss.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
